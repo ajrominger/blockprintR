014dd1792b02ad994ce015477f428d1897326eaa
ajrominger <ajrominger@gmail.com>
2022-07-11 18:20:03 +0000

    Got kmeans approach working

--- blockprint.R
+++ blockprint.R
@@ -13,24 +13,19 @@ y <- image_crop(y, '400x600+0+111')
 r <- as.raster(y)
 colz <- as.vector(r)
 
-# hcl space
-hclColz <- as(sRGB(t(col2rgb(colz) / 255)), 'polarLUV')
-i <- sample(nrow(coords(hclColz)), size = 6000)
-
-rt <- coords(hclColz)[i, 2:3]
-xy <- cbind(x = rt[, 1] * cos(rt[, 2]), y = rt[, 1] * sin(rt[, 2]))
-
-plot(sqrt(abs(xy)) * sign(xy))
-plot(xy)
+# LAB space
+labColz <- coords(as(sRGB(t(col2rgb(colz)) / 255), 'LAB'))
 
 
-plot(sqrt(coords(hclColz)[i, 2:3]))
+# clusters
+k <- kmeans(labColz, centers = 12)
 
+# new colors from clusters
+newColz <- hex(LAB(k$centers))[k$cluster]
 
-k <- kmeans(coords(), centers = 8)
+# make a new raster of the image
+newImg <- as.raster(matrix(newColz, nrow = nrow(r), byrow = TRUE))
 
-# here's the centers
-k$centers
-
-# now just need to convert to colors and re-make the raster image
+# convert to image magick class image
+image_read(newImg)
 
